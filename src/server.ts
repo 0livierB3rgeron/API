1cb788b0d465333e5948fe2fb05b74bcd5c7ade8
olivier <olivierbergeron@outlook.com>
2023-12-20 09:00:45 -0500

    ajout de documentation Swagger pour API

--- src/server.ts
+++ src/server.ts
@@ -87,6 +87,18 @@ app.get('/users', (_: Request, res: Response) => {
   return res.sendFile('users.html', { root: viewsDir });
 });
 
+// rend disponible la documentation de l'interface logicielle
+app.get('/api-docs/', async (req, res) => {
+  res.set('Content-Security-Policy', 'script-src blob:');
+  res.set('Content-Security-Policy', 'worker-src blob:');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// redirige vers api-docs
+app.get('/', (req, res) => {
+  res.redirect('/api-docs');
+});
+
 
 // **** Export default **** //
 
